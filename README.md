c6b06ce9a126f25924cb329580a73a6f2dbcadb7
manasa-kalluru <mns90852@gmail.com>
2023-11-01 23:58:01 -0400

    nested react elements

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 **CROSSORIGIN** attribute: used in HTML to specify how web browsers should handle cross-origin requests for certain resources, such as images, fonts, and scripts. Properly configuring the crossorigin attribute helps prevent security issues and ensures resources from different origins are loaded safely and correctly within your web pages.
+**React.createElement**: returns an object
+The order of importing in a html files ihas to be css, react and JS since JS files have functions that refers to react.
--- react/App.js
+++ react/App.js
@@ -1,37 +1,35 @@
-// Manipulate the HTML DOM using Javscript
-// const heading = document.createElement("h1");
-// heading.innerHTML = "Namaste Everyone";
-// const root = document.getElementById("root");
-// root.appendChild(heading);
-
-// Manipulate the HTML DOM using React
-
-// Create nested React Elements
 const heading = React.createElement(
+  "h1",
+  { id: "heading"},
+  "Hello world from React!"
+);
+
+const heading1 = React.createElement(
     "h1",
     {
-      id: "title",
+      id: "parent",
       style: {
         background:"red",
       },
       className:"title"
     },
-    "heading"
-  );
-  const heading1 = React.createElement(
-    "h1",
-    {
-      id: "title",
-    },
     "heading1"
   );
+  const heading2 = React.createElement(
+    "h1",
+    {
+      id: "child",
+    },
+    "heading2"
+  );
   
+  // Create nested React Elements
   const container = React.createElement(
     "div",
     {
       id: "container",
     },
-    [heading, heading1]
+    [heading, heading1, heading2]
   );
   
   // create root using createRoot
